77debac9a9c8c25a1ef97c703a6079fbaa7bede7
claragordon <cgordon1@uw.edu>
2026-02-24 23:32:55 +0000

    Add some run metadata to repo.

--- scripts/train_dlrm.sh
+++ scripts/train_dlrm.sh
@@ -3,5 +3,5 @@ python -m scripts.train_dlrm \
   --hash_size 131072 \
   --emb_dim 64 \
   --batch_size 4096 \
-  --epochs 1 \
-  --out_dir runs/dlrm_baseline_day2_2m
+  --epochs 3 \
+  --out_dir runs/dlrm_baseline_day2_2m_3e
